700c33482b90f566a255c74276f49074de008e88
mikececco <mikececco2000@gmail.com>
2023-02-18 19:36:58 +0100

    Added and moved date module from app to date.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express'); //used to build our app and server
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 
@@ -13,16 +14,6 @@ app.use(express.static('public'))
 
 
 app.get('/', function(req, res){
-
-    let today = new Date();
-
-    let options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    }
-
-    let day = today.toLocaleDateString('en-US', options);
 
     res.render('list', {
         kindOfDay: day,
