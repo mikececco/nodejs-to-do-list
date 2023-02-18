55f8a2abbc419fe3255684d3e58062a4865bda02
mikececco <mikececco2000@gmail.com>
2023-02-18 16:01:31 +0100

    Refractored list.ejs list item and added capitalize first letter function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-var items = [];
+var items = ['Buy food', 'Cook food', 'Eat food'];
 
 
 app.set('view engine', 'ejs'); //use ejs as view engine
@@ -30,7 +30,7 @@ app.get('/', function(req, res){
 })
 
 app.post('/', function(req, res){
-    var item = req.body.newItem;
+    var item = capitalizeFirstLetter(req.body.newItem);
     items.push(item); //to use body, need to have body-parser
     // console.log(item);
     res.redirect('/'); //trigger app.get
@@ -40,3 +40,7 @@ app.post('/', function(req, res){
 app.listen(3000, function () {
     console.log('Server started on port 3000');
 })
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
